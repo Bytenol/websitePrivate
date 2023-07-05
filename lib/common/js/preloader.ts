545bbb377302ad8e426a25d92e4d14cf0f1a4297
runtimeTerror418 <ccosmos416@gmail.com>
2023-07-05 15:50:30 -0700

    initial commit - added home page

---
+++ lib/common/js/preloader.ts
@@ -0,0 +1,12 @@
+const Preloader = {
+
+    loadImage(src) {
+        const img = new Image();
+        img.src = src;
+        return new Promise((resolve) => {
+            img.addEventListener("load", e => resolve(e));
+        })
+    }
+};
+
+export {Preloader}
